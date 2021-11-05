ce239d473d907449fd448d8e2b89cd8dd036e7de
gustavo <gustavo.vtamayo@outlook.com>
2021-11-05 17:50:08 -0600

    first commit

---
+++ Register/Model/Login.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Register.Model
+{
+    public class Login
+    {
+        public string usuario { get; set; }
+        public string pass { get; set; }
+    }
+    
+}
---
+++ Register/Model/Registro.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Register.Model
+{
+    public class Registro
+    {
+        public string usuarioRegistro { get; set; }
+        public string passRegistro { get; set; }
+    }
+}
